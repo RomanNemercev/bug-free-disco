798266fdd5e840a07794c31b941c15cb440dde8e
роман немерцев <nemercevroman@gmail.com>
2024-11-26 22:50:32 +0300

    1

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Добро пожаловать на домашнюю страницу</h1>
+        <AppAlert />
+    </div>
+</template>
+
+<script setup lang="ts">
+import '~/assets/css/first.css';
+import AppAlert from "~/components/AppAlert.vue";
+</script>
+
+<style scoped>
+h1 {
+    color: var(--color-primary);
+}
+</style>
