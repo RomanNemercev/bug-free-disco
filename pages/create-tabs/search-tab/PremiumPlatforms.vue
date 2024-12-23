f17115df90feaae5177f3f808079032971acee69
роман немерцев <nemercevroman@gmail.com>
2024-12-23 21:16:22 +0300

    complete add cards, starting configurating basket fnc

--- pages/create-tabs/search-tab/PremiumPlatforms.vue
+++ pages/create-tabs/search-tab/PremiumPlatforms.vue
@@ -14,6 +14,30 @@ const optionsData = [
         description: "Вакансия висит в топе в течении 7 дней, размещается на 7 дней."
     }
 ]
+
+const ratesData = [
+    {
+        id: "rates-1",
+        title: "Стандарт (1 490 ₽)",
+        description: "Стандартная вакансия, размещается на 30 дней."
+    },
+    {
+        id: "rates-2",
+        title: "С автоподнятием (4 390 ₽)",
+        description: "Вакансия поднимается каждые 3 дня, в течении 30 дней."
+    },
+    {
+        id: "rates-3",
+        title: "Бизнес (14 640 ₽)",
+        description: "Вакансия висит в топе в течении 7 дней, размещается на 7 дней."
+    },
+    {
+        id: "rates-4",
+        title: "Бизнес",
+        description: "Вакансия висит в топе в течении 7 дней, размещается на 7 дней."
+    }
+]
+
 const dropItems = ['Импорт публикаций', 'Отвязать профиль'];
 </script>
 
@@ -146,7 +170,7 @@ const dropItems = ['Импорт публикаций', 'Отвязать про
               class="flex-1 basis-[200px] max-w-[281.5px] p-25px bg-white rounded-fifteen flex flex-col min-h-[404px]">
                 <div class="flex justify-between mb-3.5">
                     <div class="flex items-center gap-2.5">
-                        <div class="youla-pic"></div>
+                        <div class="youla-pic bg-img"></div>
                         <p class="text-sm font-medium text-slate-custom">youla.ru</p>
                     </div>
                     <DotsDropdonw :items="dropItems" />
@@ -168,14 +192,119 @@ const dropItems = ['Импорт публикаций', 'Отвязать про
         <p class="text-xl font-semibold text-space mb-5px">Магазин Jobly </p>
         <p class="text-sm font-normal text-slate-custom mb-25px">Вакансия будет опубликована с&nbsp;аккаунта Jobly, все
             остальное остается прежним</p>
+        <div class="w-full gap-x-25px flex">
+            <div class="flex gap-15px mb-35px max-w-[875px] flex-wrap w-full">
+                <!-- Первая карточка -->
+                <div
+                  class="flex-1 basis-[210px] max-w-[281.5px] p-25px bg-white rounded-fifteen flex flex-col min-h-[248px]">
+                    <div class="flex items-center gap-2.5 mb-3.5">
+                        <svg-icon name="hh" width="41" height="40" />
+                        <p class="text-sm font-medium text-slate-custom">hh.ru</p>
+                    </div>
+                    <div class="w-full h-[1px] bg-athens mb-3.5"></div>
+                    <p class="text-sm font-medium text-space mb-3.5">Тарифный план:</p>
+                    <MultiDropdown :options="ratesData" :selected="ratesData[0]" class="mb-15px" variant="selected" />
+                    <UiButton variant="action" size="action" class="mt-auto">В корзину</UiButton>
+                </div>
+                <!-- Вторая карточка -->
+                <div
+                  class="flex-1 basis-[210px] max-w-[281.5px] p-25px bg-white rounded-fifteen flex flex-col min-h-[248px]">
+                    <div class="flex items-center gap-2.5 mb-3.5">
+                        <div class="avito-pic bg-img"></div>
+                        <p class="text-sm font-medium text-slate-custom">avito.ru</p>
+                    </div>
+                    <div class="w-full h-[1px] bg-athens mb-3.5"></div>
+                    <p class="text-sm font-medium text-space mb-3.5">Тарифный план:</p>
+                    <MultiDropdown :options="ratesData" :selected="ratesData[0]" class="mb-15px" variant="selected" />
+                    <UiButton variant="action" size="action" class="mt-auto">В корзину</UiButton>
+                </div>
+                <!-- Третья карточка -->
+                <div
+                  class="flex-1 basis-[210px] max-w-[281.5px] p-25px bg-white rounded-fifteen flex flex-col min-h-[248px]">
+                    <div class="flex items-center gap-2.5 mb-3.5">
+                        <div class="rabota-pic bg-img"></div>
+                        <p class="text-sm font-medium text-slate-custom">rabota.ru</p>
+                    </div>
+                    <div class="w-full h-[1px] bg-athens mb-3.5"></div>
+                    <p class="text-sm font-medium text-space mb-3.5">Тарифный план:</p>
+                    <MultiDropdown :options="ratesData" :selected="ratesData[0]" class="mb-15px" variant="selected" />
+                    <UiButton variant="action" size="action" class="mt-auto">В корзину</UiButton>
+                </div>
+                <!-- Четвертая карточка -->
+                <div
+                  class="flex-1 basis-[210px] max-w-[281.5px] p-25px bg-white rounded-fifteen flex flex-col min-h-[248px]">
+                    <div class="flex items-center gap-2.5 mb-3.5">
+                        <svg-icon name="zarplata" width="41" height="40" />
+                        <p class="text-sm font-medium text-slate-custom">rabota.ru</p>
+                    </div>
+                    <div class="w-full h-[1px] bg-athens mb-3.5"></div>
+                    <p class="text-sm font-medium text-space mb-3.5">Тарифный план:</p>
+                    <MultiDropdown :options="ratesData" :selected="ratesData[0]" class="mb-15px" variant="selected" />
+                    <UiButton variant="action" size="action" class="mt-auto">В корзину</UiButton>
+                </div>
+                <!-- Пятая карточка -->
+                <div
+                  class="flex-1 basis-[210px] max-w-[281.5px] p-25px bg-white rounded-fifteen flex flex-col min-h-[248px]">
+                    <div class="flex items-center gap-2.5 mb-3.5">
+                        <svg-icon name="superjob" width="41" height="40" />
+                        <p class="text-sm font-medium text-slate-custom">superjob.ru</p>
+                    </div>
+                    <div class="w-full h-[1px] bg-athens mb-3.5"></div>
+                    <p class="text-sm font-medium text-space mb-3.5">Тарифный план:</p>
+                    <MultiDropdown :options="ratesData" :selected="ratesData[0]" class="mb-15px" variant="selected" />
+                    <UiButton variant="action" size="action" class="mt-auto">В корзину</UiButton>
+                </div>
+                <!-- Шестая карточка -->
+                <div
+                  class="flex-1 basis-[210px] max-w-[281.5px] p-25px bg-white rounded-fifteen flex flex-col min-h-[248px]">
+                    <div class="flex items-center gap-2.5 mb-3.5">
+                        <div class="youla-pic bg-img"></div>
+                        <p class="text-sm font-medium text-slate-custom">youla.ru</p>
+                    </div>
+                    <div class="w-full h-[1px] bg-athens mb-3.5"></div>
+                    <p class="text-sm font-medium text-space mb-3.5">Тарифный план:</p>
+                    <MultiDropdown :options="ratesData" :selected="ratesData[0]" class="mb-15px" variant="selected" />
+                    <UiButton variant="action" size="action" class="mt-auto">В корзину</UiButton>
+                </div>
+                <!-- Седьмая карточка -->
+                <div
+                  class="flex-1 basis-[210px] max-w-[281.5px] p-25px bg-white rounded-fifteen flex flex-col min-h-[248px]">
+                    <div class="flex items-center gap-2.5 mb-3.5">
+                        <svg-icon name="careerist" width="41" height="40" />
+                        <p class="text-sm font-medium text-slate-custom">careerist.ru</p>
+                    </div>
+                    <div class="w-full h-[1px] bg-athens mb-3.5"></div>
+                    <p class="text-sm font-medium text-space mb-3.5">Тарифный план:</p>
+                    <MultiDropdown :options="ratesData" :selected="ratesData[0]" class="mb-15px" variant="selected" />
+                    <UiButton variant="action" size="action" class="mt-auto">В корзину</UiButton>
+                </div>
+            </div>
+            <div>
+                <div class="flex flex-col p-25px justify-center gap-y-15px bg-white rounded-fifteen">
+                    <p class="text-xl font-semibold text-space">Ваша корзина пуста</p>
+                    <p class="text-sm font-normal text-slate-custom">Пока здесь пусто</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-.youla-pic {
-    background-image: url('@/assets/img/youla.svg');
+.bg-img {
     background-size: cover;
     width: 41px;
     height: 40px;
+}
+
+.youla-pic {
+    background-image: url('@/assets/img/youla.svg');
+}
+
+.avito-pic {
+    background-image: url('@/assets/img/avito.svg');
+}
+
+.rabota-pic {
+    background-image: url('@/assets/img/rabota-ru.svg');
 }
 </style>
